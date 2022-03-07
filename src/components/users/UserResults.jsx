39467161b85b7716c41f429507cf6cd07b8822c1
Tenzin Dekyong <dhontenzin@gmail.com>
2022-03-07 19:26:46 +0100

    useContext init

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,30 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`, // using the token github
-      },
-    });
-
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => {
-          return <h3>{user.login}</h3>;
+          return <UserItem key={user.id} user={user} />;
         })}
       </div>
     );
