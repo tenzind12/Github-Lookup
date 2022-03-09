d237cfca36f87e6b2e20386e30d1bff2688e50bc
Tenzin Dekyong <dhontenzin@gmail.com>
2022-03-09 06:59:46 +0100

    User.jsx data render

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,9 +1,13 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 import GithubContext from '../../context/github/GithubContext';
 
 function User() {
-  const { getUser, user } = useContext(GithubContext);
+  const { getUser, user, loading } = useContext(GithubContext);
+  console.log(user);
 
   const params = useParams(); //import useParams() to get login from url
 
@@ -11,7 +15,67 @@ function User() {
     getUser(params.login);
   }, []);
 
-  return <div>{user.login}</div>;
+  if (loading) return <Spinner />;
+
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back To Search
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt={name} />
+              </figure>
+              <div className="card-body justify-end">
+                <div>
+                  <h2 className="card-title mb-0">{name}</h2>
+                  <p>{login}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge bagdge-success">{type}</div>
+                {hireable && <div className="mx-1 badge badge-info">Hireable</div>}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a href={html_url} target="_blank" rel="noreferrer" className="btn btn-outline">
+                  Visit Github Profile
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default User;
